d494d0ad4a6da1d97b501b07db78dd8219004e6f
Santiago Cuervo <scuervo91@gmail.com>
2021-01-27 09:55:49 -0500

    add post example

--- fastapi/1-intro/main.py
+++ fastapi/1-intro/main.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from fastapi import FastAPI 
-
+from typing import Optional
+from pydantic import BaseModel
 #predifined items
 class ModelItems(str,Enum):
     santi = 910821
@@ -9,6 +10,14 @@ class ModelItems(str,Enum):
 
 app = FastAPI() 
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+class  Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float 
+    tax: Optional[float] = None
+    
 
 @app.get("/items/mine")
 async def read_item_mine():
@@ -25,3 +34,37 @@ async def read_model(model_id: ModelItems):
     else:
         return {"model_id": model_id, "message": "Juli Model"}
     
+@app.get("/db/")
+async def read_db(skip: int, limit: int):
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/optional/{item_id}")
+async def read_optional(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+## Pydantic POST Request
+
+@app.post("/items/{item_id}")
+async def create_item(item: Item, item_id: int):
+    return {'item_id':item_id,**item.dict()}
